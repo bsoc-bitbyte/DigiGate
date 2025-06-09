ad6b68dc17bc3e1e41ff186cfff6848aa798a3c8
harsh-bhadauria <hsb.harsh070@gmail.com>
2025-06-09 13:34:53 +0530

    turned off dynamic color

--- app/src/main/java/com/tpc/digigate/ui/theme/Theme.kt
+++ app/src/main/java/com/tpc/digigate/ui/theme/Theme.kt
@@ -37,7 +37,7 @@ private val DarkColorScheme = darkColorScheme(
 fun DigiGateTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
