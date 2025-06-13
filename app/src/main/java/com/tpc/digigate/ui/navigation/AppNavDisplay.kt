5cd376ac8ef11ed45e2bc0e9d6b2a35b5e321348
harsh-bhadauria <hsb.harsh070@gmail.com>
2025-06-13 14:26:14 +0530

    Added files for navigation issues

---
+++ app/src/main/java/com/tpc/digigate/ui/navigation/AppNavDisplay.kt
@@ -0,0 +1,8 @@
+package com.tpc.digigate.ui.navigation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppNavDisplay(){
+
+}
---
+++ app/src/main/java/com/tpc/digigate/ui/screens/profile/ProfileScreen.kt
@@ -0,0 +1,9 @@
+package com.tpc.digigate.ui.screens.profile
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProfileScreen(modifier: Modifier = Modifier){
+
+}
